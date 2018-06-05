665178510b86ea56be6d8a86e6696631ffbaee5e
marshygeek <lester0578@gmail.com>
2018-06-05 19:44:50 +0300

    Added spider for scraping books titles. Changed Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# books_scraper
+Resulting file will be stored in books/books.json
+# Setup
+1. Install Python 3.6
+2. `pip install -r requirements.txt`
+# Launch
+1. Open books folder
+2. Enter command `scrapy crawl books`.
+
+It will take ~1 minute to execute
+# Possible issues
+When running the spider on Windows 10, it is possible that the next error will occur:
+`ImportError: No module named win32api.`
+Enter this command to fix it:
+`pip install pypiwin32`
--- books/books/spiders/books_spider.py
+++ books/books/spiders/books_spider.py
@@ -1,11 +1,23 @@
-# -*- coding: utf-8 -*-
 import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 
 
-class BooksSpiderSpider(scrapy.Spider):
-    name = 'books_spider'
-    allowed_domains = ['books.toscrape.com']
+class BooksSpider(scrapy.Spider):
+    name = 'books'
     start_urls = ['http://books.toscrape.com/']
 
     def parse(self, response):
-        pass
+        common_part = '//div/div/div/div/section/div/{}'
+        for book in response.xpath(common_part.format('ol/li')):
+            yield {'title': book.xpath('./article/h3/a/@title').extract_first()}
+
+        next_page_url = response.xpath(common_part.format('div/ul/li[@class="next"]/a/@href')).extract_first()
+        if next_page_url is not None:
+            yield response.follow(next_page_url)
+
+
+if __name__ == '__main__':
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(BooksSpider)
+    process.start()
